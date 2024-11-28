5f460e9510bbb172ca29dd9ebc8190962554b1b3
defycab <love.bellinder@gmail.com>
2024-11-28 15:32:21 +0100

    fixing offers on offers.tsx

--- src/features/main/components/offers.tsx
+++ src/features/main/components/offers.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { useState } from "react";
@@ -10,10 +10,45 @@ export type Props = {
 export function Offers({ litersOfPaint }: Props) {
   const [isVisible, setIsVisible] = useState(false);
   return (
-    <div className="flex flex-col w-60 justify-center">
+    <div className="flex flex-col w-64 justify-center border-2 border-slate-200">
       <main className="flex flex-col justify-center items-center bg-slate-50">
+        <h3 className="text-2xl mb-2 underline">Offers</h3>
+        <article
+          onClick={() => {
+            setIsVisible(true);
+          }}
+          className="cursor-pointer flex flex-col justify-center items-center border-2 border-slate-200 my-2"
+        >
+          <Image src="/bauhaus.png" width={130} height={130} alt="bauhaus" />
+          <div className="flex flex-row">
+            <p>{litersOfPaint} liters for</p>
+            <p className="text-red-600">&nbsp;696</p>
+            <p>&nbsp;SEK</p>
+          </div>
+        </article>
+        <article className="cursor-pointer flex flex-col justify-center items-center border-2 border-slate-200 my-2">
+          <Image
+            src="/k-rauta-main.png"
+            width={130}
+            height={130}
+            alt="bauhaus"
+          />
+          <div className="flex flex-row">
+            <p>{litersOfPaint} liters for</p>
+            <p className="text-red-600">&nbsp;729</p>
+            <p>&nbsp;SEK</p>
+          </div>
+        </article>
+        <article className="cursor-pointer flex flex-col justify-center items-center border-2 border-slate-200 my-2">
+          <Image src="/hornbach.png" width={130} height={130} alt="bauhaus" />
+          <div className="flex flex-row">
+            <p>{litersOfPaint} liters for</p>
+            <p className="text-red-600">&nbsp;749</p>
+            <p>&nbsp;SEK</p>
+          </div>
+        </article>
         <div className={isVisible ? "visible" : "invisible"}>
-          <article className="p-4 border-2 border-slate-200 h-64 flex flex-col justify-center items-center w-60">
+          <article className="p-4 border-2 border-slate-200 h-64 flex flex-col justify-center items-center w-64">
             <Image src="/bauhaus.png" width={130} height={130} alt="bauhaus" />
             <p className="text-sm font-semibold mt-2">
               You need {litersOfPaint} liters of paint
