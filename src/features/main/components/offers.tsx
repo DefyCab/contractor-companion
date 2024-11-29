695dd831df5659a1398266f150185b5d7f4be4cc
defycab <love.bellinder@gmail.com>
2024-11-29 04:52:58 +0100

    styling offer page

--- src/features/main/components/offers.tsx
+++ src/features/main/components/offers.tsx
@@ -19,43 +19,56 @@ export function Offers({ litersOfPaint }: Props) {
           }}
           className="cursor-pointer flex flex-col justify-center items-center border-2 border-slate-200 my-2"
         >
-          <Image src="/bauhaus.png" width={130} height={130} alt="bauhaus" />
-          <div className="flex flex-row">
-            <p>{litersOfPaint} liters for</p>
-            <p className="text-red-600">&nbsp;696</p>
-            <p>&nbsp;SEK</p>
-          </div>
-        </article>
-        <article className="cursor-pointer flex flex-col justify-center items-center border-2 border-slate-200 my-2">
           <Image
-            src="/k-rauta-main.png"
+            className="my-1 p-1"
+            src="/bauhaus.png"
             width={130}
             height={130}
             alt="bauhaus"
           />
           <div className="flex flex-row">
-            <p>{litersOfPaint} liters for</p>
-            <p className="text-red-600">&nbsp;729</p>
-            <p>&nbsp;SEK</p>
+            <p className="ml-2">{litersOfPaint} liters for</p>
+            <p className="text-red-600">&nbsp;696</p>
+            <p className="mr-2">&nbsp;SEK</p>
           </div>
         </article>
         <article className="cursor-pointer flex flex-col justify-center items-center border-2 border-slate-200 my-2">
-          <Image src="/hornbach.png" width={130} height={130} alt="bauhaus" />
+          <Image
+            className="my-1 p-1"
+            src="/k-rauta-main.png"
+            width={110}
+            height={110}
+            alt="k-rauta"
+          />
           <div className="flex flex-row">
-            <p>{litersOfPaint} liters for</p>
+            <p className="ml-2">{litersOfPaint} liters for</p>
+            <p className="text-red-600">&nbsp;729</p>
+            <p className="mr-2">&nbsp;SEK</p>
+          </div>
+        </article>
+        <article className="cursor-pointer flex flex-col justify-center items-center border-2 border-slate-200 my-2">
+          <Image
+            className="my-1 p-1"
+            src="/hornbach.png"
+            width={130}
+            height={130}
+            alt="hornbach"
+          />
+          <div className="flex flex-row">
+            <p className="ml-2">{litersOfPaint} liters for</p>
             <p className="text-red-600">&nbsp;749</p>
-            <p>&nbsp;SEK</p>
+            <p className="mr-2">&nbsp;SEK</p>
           </div>
         </article>
         <div className={isVisible ? "visible" : "invisible"}>
           <article className="p-4 border-2 border-slate-200 h-64 flex flex-col justify-center items-center w-64">
             <Image src="/bauhaus.png" width={130} height={130} alt="bauhaus" />
             <p className="text-sm font-semibold mt-2">
-              You need {litersOfPaint} liters of paint
+              You got {litersOfPaint} liters of paint!
             </p>
             <div className="flex flex-row justify-center">
               <div className="p-4">
-                <p className="text-[0.5rem]">1 liter x 2</p>
+                <p className="text-[0.6rem]">1 liter x 2</p>
                 <Image
                   src="/bauhaus-becker-1.jpg"
                   width={40}
@@ -64,7 +77,7 @@ export function Offers({ litersOfPaint }: Props) {
                 />
               </div>
               <div className="p-4">
-                <p className="text-[0.5rem]">3 liter x 2</p>
+                <p className="text-[0.6rem]">3 liter x 2</p>
                 <Image
                   src="/bauhaus-beckers-3.jpg"
                   width={40}
